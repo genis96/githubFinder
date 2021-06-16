fd42b32af79121c9b1335e71e71320420fa0a491
Genis Bardales <genis96@yahoo.com>
2021-06-16 19:14:39 -0400

    routers next

--- src/App.js
+++ src/App.js
@@ -3,30 +3,51 @@ import './App.css';
 import  Navbar from './components/layout/Navbar'
 import Users from './components/users/Users';
 import Search from './components/users/Search';
+import Alert from './components/layout/Alert';
 import axios from 'axios'; 
 
 // class based component 
 class App extends Component{
   state = {
     users: [],
-    loading: false
+    loading: false,
+    alert: null
   }
 
   // searching github users
   searchUsers = async text => {
+    this.setState({ loading: true });
     const res = await axios.get(
       `https://api.github.com/search/users?q=${text}&client_id=${process.env.REACT_APP_GITHUB_CLIENT_ID}&client_secret=${process.env.REACT_APP_GITHUB_CLIENT_SECRET}`
       );
     this.setState({ users: res.data.items, loading: false });
   }
 
+  // clears users from state
+  clearUsers = () => this.setState({ users: [], loading: false });
+
+  // alert
+  setAlert = (msg, type) => {
+    this.setState({ alert: { msg, type } });
+    setTimeout(() => this.setState({ alert: null }), 3000);
+  }
+
   render() {
+
+    const { users, loading } = this.state;
+
     return (
       <div className="App">
         <Navbar />
         <div className='container'>
-          <Search searchUsers={ this.searchUsers } />
-          <Users loading={ this.state.loading } users={ this.state.users } />
+          <Alert alert={ this.state.alert }/>
+          <Search 
+          searchUsers={ this.searchUsers } 
+          clearUsers={ this.clearUsers } 
+          showClear={ users.length > 0 ? true : false } 
+          setAlert={this.setAlert}
+          />
+          <Users loading={ loading } users={ users } />
         </div>
       </div>
     );
--- src/components/users/Search.js
+++ src/components/users/Search.js
@@ -1,21 +1,36 @@
 import React, { Component } from 'react'
+import PropTypes from 'prop-types'
 
 export class Search extends Component {
     state = {
         text: ''
     }
 
+    static propTypes = {
+        searchUsers: PropTypes.func.isRequired,
+        clearUsers: PropTypes.func.isRequired,
+        showClear: PropTypes.bool.isRequired,
+        setAlert: PropTypes.func.isRequired
+    }
+
     // were passing in the text here
     onSubmit = (e) => {
         e.preventDefault();
-        this.props.searchUsers(this.state.text);
-        this.setState({ text: '' });
+        if(this.state.text === '') {
+            this.props.setAlert('Please enter something', 'light')
+        } else {
+            this.props.searchUsers(this.state.text);
+            this.setState({ text: '' });
+        }
     }
 
     onChange = (e) => this.setState({ [e.target.name]: e.target.value });
     
     // we are submitting the form using onsubmit here
     render() {
+
+        const { showClear, clearUsers } = this.props;
+
         return (
             <div>
                 <form onSubmit={this.onSubmit} className='form'>
@@ -30,9 +45,10 @@ export class Search extends Component {
                     type='submit' 
                     value='Search' 
                     className='btn btn-dark btn-block'
-                    
                     />
                 </form>
+                { showClear && <button className='btn btn-light btn-block' onClick={ clearUsers }> Clear </button> }
+                
             </div>
         )
     }
